c80c249b3da0965eeb9b437b60dd3f683c9d667f
matthewc423 <90159010+matthewc423@users.noreply.github.com>
2022-08-22 13:42:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#!usr/bin/env pypy
+
+
+import argparse  
+import importlib
+import os
+
+from scene import Scene
+from engine import RenderEngine
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("scene", help = "Path to scene file (without.py extension)")
+	args = parser.parse_args()
+	mod = importlib.import_module(args.scene)
+
+	scene = Scene(mod.CAMERA, mod.OBJECTS, mod.LIGHTS, mod.WIDTH, mod.HEIGHT)
+	engine = RenderEngine()
+	image = engine.render(scene)
+	
+	os.chdir(os.path.dirname(os.path.abspath(mod.__file__)))
+	with open(mod.RENDERED_IMG, "w") as img_file:
+		image.write_ppm(img_file)
+
+if __name__ == "__main__":
+	main()
---
+++ sphere.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+class Sphere:
+	"""Sphere has center, radius, and material"""
+
+	def __init__(self, center, radius, material):
+		self.center = center
+		self.radius = radius
+		self.material = material
+
+	def intersects(self, ray):
+		"""Checks if ray intersects this sphere. Returns distance to intersection or None if no intersection"""
+		sphere_to_ray = ray.origin - self.center
+		a = 1
+		b = 2 * ray.direction.dot_product(sphere_to_ray)
+		c = sphere_to_ray.dot_product(sphere_to_ray) - self.radius * self.radius
+		discriminant = b * b - 4 * a * c 
+
+		if discriminant >= 0:
+			dist = (-b - sqrt(discriminant)) / 2
+			if dist > 0:
+				return dist 
+
+		return None
+
+	def normal(self, surface_point):
+		"""Retirms surface normal to the point on sphere's surface"""
+		return (surface_point - self.center).normalize()
